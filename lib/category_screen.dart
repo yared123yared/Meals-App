d34ea18e09c228d0932c11feebda668c50a7bc16
=yared123yared <=yaredyaya16@gmail.com>
2021-01-06 11:40:51 +0300

    initial commit

---
+++ lib/category_screen.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:shopping_app/category_item.dart';
+
+import 'dummy_data.dart';
+
+class CategoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body:GridView(
+      padding: EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.id ,catData.title, catData.color)).toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 100,
+        childAspectRatio: 3/2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+
+      ),
+
+    ) ,) ;
+  }
+}
