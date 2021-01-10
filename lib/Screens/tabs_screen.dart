3b70d65e8736a9117c3a6687d92c000536e84309
=yared123yared <=yaredyaya16@gmail.com>
2021-01-10 03:09:42 +0300

    Adding a bottom tap bar

--- lib/Screens/tabs_screen.dart
+++ lib/Screens/tabs_screen.dart
@@ -9,27 +9,58 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  List<Widget> _pages=[
+    CategoryScreen(),
+    FavoriteScreen()
+  ];
+  int _selectedPageIndex = 0 ;
+
+  void _selectPage(int index) {
+    this.setState(() {
+
+      this._selectedPageIndex=index;
+    });
+  }
+  String get getTitle{
+    String value='';
+
+    if(this._selectedPageIndex==0){
+//      Category section
+    value='Categories';
+    }else{
+      value='Favorities';
+    }
+
+    return value;
+  }
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2, child: Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Meals'),
-        bottom: TabBar(
-          tabs: [
-            Tab(icon: Icon(Icons.category), text:'Categories'),
-            Tab(icon: Icon(Icons.star), text:'Favorites'),
+        title: Text('$getTitle'),
 
-
-          ],
-        ),
       ),
-      body: TabBarView(
-        children: [
-          CategoryScreen(),
-          FavoriteScreen()
-
+      body: _pages[this._selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor:Theme.of(context).accentColor,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectedPageIndex,
+        onTap: this._selectPage,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+            backgroundColor: Theme.of(context).primaryColor
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text('Favorities'),
+              backgroundColor: Theme.of(context).primaryColor
+          )
         ],
       ),
-    ));
+    );
   }
 }
